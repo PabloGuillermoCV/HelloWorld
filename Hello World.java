f45fd731afffd486e3e3ad42bb87664558830e3b
Pablo Guillermo <pgcb123@gmail.com>
2019-03-13 09:19:16 -0300

    subiendo desde Branch-Pablo
    cree una nueva rama y hice cambios

--- Hello World.java
+++ Hello World.java
@@ -2,6 +2,7 @@ public class HelloWorld{
 	
 	public static void main(String args[]){
 		system.out.println("Probando git con Pablo y Tomas");
+		system.out.println("Probando ahora con branch Branch-Pablo");
 	}
 
 }
